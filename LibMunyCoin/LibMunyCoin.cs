b328fbeb2dc93d61517adb706d4f7bb5c2e2a82f
Kevin Smith <kevin@muny.us>
2017-12-14 23:26:44 -0500

    initial commit

---
+++ LibMunyCoin/LibMunyCoin.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LibMunyCoin
+{
+    public class LibMunyCoin
+    {
+        public static string testing = "testing 123";
+    }
+}
---
+++ MunyCoinClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LibMunyCoin;
+
+namespace MunyCoinClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("project setup [" + LibMunyCoin.LibMunyCoin.testing + "]");
+
+            
+        }
+    }
+}
